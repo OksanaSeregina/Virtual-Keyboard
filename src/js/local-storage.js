2f5e2278b1d14bee8150fefb5b754f6f5f7ed839
Oksana <seregina.oksana.1987@gmail.com>
2022-05-10 16:56:15 +0300

    feat: update build

--- src/js/local-storage.js
+++
@@ -1,7 +0,0 @@
-export function set(name, value) {
-  window.localStorage.setItem(name, JSON.stringify(value));
-}
-
-export function get(name, subVal = null) {
-  return JSON.parse(window.localStorage.getItem(name) || subVal);
-}
